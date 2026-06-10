9307364216b8aea61cfa354898dede79684a0f69
24cse-deepika <deepikagarg630@gmail.com>
2026-06-10 10:23:52 +0530

    Initial Express setup with middleware and route mounting

---
+++ dayforge-backend/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+// Import routes
+const authRoutes = require('./routes/auth');
+const taskRoutes = require('./routes/tasks');
+
+// Use routes
+app.use('/api/auth', authRoutes);
+app.use('/api/tasks', taskRoutes);
+
+// 404 handler
+app.use((req, res, next) => {
+  res.status(404).json({ error: 'Not Found' });
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Something went wrong!' });
+});
+
+module.exports = app;
